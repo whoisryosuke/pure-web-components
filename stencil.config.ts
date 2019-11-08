414d390d95e546aba387d99a1155c7f99e60aab1
Ryosuke <ryosuke.san.hana@gmail.com>
2019-11-07 17:20:03 -0800

    :shirt: prettier run

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,18 +1,21 @@
-import { Config } from '@stencil/core';
+import { Config } from "@stencil/core";
+import { sass } from "@stencil/sass";
 
 export const config: Config = {
-  namespace: 'pure-web-components',
+  namespace: "pure-web-components",
   outputTargets: [
     {
-      type: 'dist',
-      esmLoaderPath: '../loader'
+      type: "dist",
+      esmLoaderPath: "../loader"
     },
     {
-      type: 'docs-readme'
+      type: "docs-readme"
     },
     {
-      type: 'www',
+      type: "www",
       serviceWorker: null // disable service workers
     }
+  ],
+
   plugins: [sass()]
 };
