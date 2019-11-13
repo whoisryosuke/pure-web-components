b4712f39318568c05138a5316bc14bcac12fd3b3
Ryosuke <ryosuke.san.hana@gmail.com>
2019-11-13 12:31:34 -0800

    :heavy_plus_sign: knobs, a11y, bg, + options addon

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,4 +1,8 @@
+import "@storybook/addon-a11y/register";
 import "@storybook/addon-actions/register";
+import "@storybook/addon-backgrounds/register";
 import "@storybook/addon-jest/register";
+import "@storybook/addon-knobs/register";
 import "@storybook/addon-notes/register";
+import "@storybook/addon-options/register";
 import "@storybook/addon-viewport/register";
--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,6 @@
 import { configure, addDecorator } from "@storybook/html";
+import { withKnobs } from "@storybook/addon-knobs";
+import { withA11y } from "@storybook/addon-a11y";
 import { withTests } from "@storybook/addon-jest";
 // import results from "../jest-test-results.json";
 
@@ -8,6 +10,15 @@ import { withTests } from "@storybook/addon-jest";
 //     filesExt: ".spec.ts"
 //   })
 // );
+
+// Add a11y support
+addDecorator(withA11y);
+
+// Add knobs to all stories
+addDecorator(withKnobs);
+
+// Theme provider wraps app
+// addDecorator(story => <pure-base>{storyFn()}</pure-base>);
 
 const req = require.context("../src", true, /\.stories\.tsx$/);
 function loadStories() {
