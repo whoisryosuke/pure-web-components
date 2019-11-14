37f26d1c74f725eeb23381cfe31712a432f8bcf2
Ryosuke <ryosuke.san.hana@gmail.com>
2019-11-13 16:28:23 -0800

    :lipstick: dark menus

--- docs/src/components/header.js
+++ docs/src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `var(--pure-color-primary)`,
+      background: `var(--pure-color-dark-background)`,
       position: "relative",
     }}
   >
