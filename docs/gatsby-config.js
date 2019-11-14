6ec24c04e27cff3487fd3c38ea27de60a9dc3a44
Ryosuke <ryosuke.san.hana@gmail.com>
2019-11-13 18:20:35 -0800

    :pencil: site info updated

--- docs/gatsby-config.js
+++ docs/gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Pure Web Components`,
+    description: `Documentation for Pure Web Components, a web component library inspired by Pure CSS`,
+    author: `@whoisryosuke`,
   },
   plugins: [
     `gatsby-transformer-json`,
